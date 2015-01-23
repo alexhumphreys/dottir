222bf6f98f3aa8d82360332473da008887728cb0
Alex Humphreys <alex@aupeo.com>
2015-01-23 11:13:27 +0100

    + replaced colors with pictures

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ $(function() {
 
   var repeatTime = 6000;
 
-  var leftColors = ['RebeccaPurple' , 'Red' , 'RosyBrown' , 'RoyalBlue' , 'SaddleBrown' , 'Salmon' , 'SandyBrown' , 'SeaGreen' , 'SeaShell' , 'Sienna' , 'Silver'];
-  var rightColors = ['SkyBlue' , 'SlateBlue' , 'SlateGray' , 'Snow' , 'SpringGreen' , 'SteelBlue' , 'Tan' , 'Teal' , 'Thistle' , 'Tomato' , 'Turquoise' , 'Violet' , 'Wheat', 'White' ];
+  var leftPics = ['l1' , 'l2' , 'l3' , 'l4' , 'l5' , 'l6' , 'l7'];
+  var rightPics = ['r1' , 'r2' , 'r3' , 'r4' , 'r5'];
 
   createTransitionImage = function(selector, array, interval, index) {
     setInterval(function() {
-      $(selector).css('background', array[index]);
+      $(selector).css('background-image', 'url(images/' + array[index] + '.jpg)');
       if (index + 1 == array.length) {
         index = 0;
       } else {
@@ -16,6 +16,6 @@ $(function() {
     }, interval);
   };
 
-  createTransitionImage('.left', leftColors, repeatTime, 0);
-  createTransitionImage('.right', rightColors, repeatTime, 0);
+  createTransitionImage('.left', leftPics, repeatTime, 0);
+  createTransitionImage('.right', rightPics, repeatTime, 0);
 });
