0468948f7f2df748e0fce23d2084fff33edf59fe
Alex Humphreys <humphreys.a@gmail.com>
2015-01-25 23:11:52 +0100

    * changed background iteration time

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 $(function() {
 
-  var repeatTime = 6000;
+  var repeatTime = 10000;
 
-  var leftPics = ['l1' , 'l2' , 'l3' , 'l4' , 'l5' , 'l6' ];
-  var rightPics = ['r1' , 'r2' , 'r3' , 'r4' , 'r5'];
+  var leftPics = ['l1', 'l2' , 'l3' , 'l4' , 'l5' , 'l6' ];
+  var rightPics = ['r2' , 'r3' , 'r4' , 'r5', 'r1'];
+
+  function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = "images/" + this + ".jpg";
+        // Alternatively you could use:
+        // (new Image()).src = this;
+    });
+  }
+
+  preload(leftPics);
+  preload(rightPics);
+
+  function changeImage(selector, path) {
+    $(selector).css('background-image', path);
+  }
 
   createTransitionImage = function(selector, array, interval, index) {
     setInterval(function() {
-      $(selector).css('background-image', 'url(images/' + array[index] + '.jpg)');
+      changeImage(selector, 'url(images/' + array[index] + '.jpg)');
       if (index + 1 == array.length) {
         index = 0;
       } else {
@@ -16,8 +31,12 @@ $(function() {
     }, interval);
   };
 
+  setTimeout(function() {
+    changeImage('.right', 'url(images/r1.jpg)');
+  }, 5000);
+
   createTransitionImage('.left', leftPics, repeatTime, 0);
   setTimeout(function() {
     createTransitionImage('.right', rightPics, repeatTime, 0);
-  }, 3000);
+  }, 5000);
 });
